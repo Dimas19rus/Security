d92b6fc140966bc4a10e89c97e116a96e866eb8f
Dimas19rus <Dimas.titan@mail.ru>
2017-06-01 23:49:01 +0700

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Abiturient h = new Abiturient {Name = "Abiturient"};
+            Prepod p =new Prepod {Name = "Prepod"};
+            Rabotodatel r =new Rabotodatel {Name = "Rabo"};
+            Dog dog = new Dog {Name = "Dog"};
+            Squirell squirell = new Squirell {Name = "Squirell"};
+            Student student =new Student {Name = "Student"};
+            List<INumericObject> list = new List<INumericObject> {h,p,r,dog,squirell,student};
+            foreach (var o in list)
+            {
+                Security.Propusk(o);
+            }
+            Security.InfoDopusk();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication3/ConsoleApplication1/Static/Security.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public static class Security
+    {
+        public static List<IDopusk> Dopusk { get; set; } = new List<IDopusk>();
+
+        public static void Propusk(INumericObject o )
+        {
+            if (o is IDopusk)
+            {
+                Dopusk.Add((IDopusk)o);
+            }
+        }
+
+        public static void InfoDopusk()
+        {
+            Console.WriteLine("Список пропущенных:");
+            foreach (var d in Dopusk)
+                Console.WriteLine("\t" + ((INumericObject)d).Name);               
+        }
+    }
+}
